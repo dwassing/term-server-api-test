1f2c66b61245ca4eee338631c32c7e7fb18dc275
Daniel Wassing <danwa223@student.liu.se>
2020-07-13 16:06:52 +0200

    Sample testing tool for debugging JSON objects.

---
+++ TermServerRestAPI/src/main/java/APItest/TermServerRestAPI/FHIRMapperTest.java
@@ -0,0 +1,41 @@
+package APItest.TermServerRestAPI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class FHIRMapperTest 
+{
+	
+	String stringTomap="";
+	
+	public static void main(String [] args)
+	{
+		BasicConfigurator.configure();
+		Logger.getLogger("ca.uhn.fhir.util.VersionUtil").setLevel(Level.OFF);
+		Logger.getLogger("ca.uhn.fhir.context.ModelScanner").setLevel(Level.OFF);
+		Logger.getRootLogger().setLevel(Level.OFF);
+		StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("/home/wassing/Desktop/parameterstest.txt"))) 
+        {
+
+            // read line by line
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+        System.out.print(sb.toString());
+        System.out.print("Result:"+new FHIRMapper(sb.toString()).getValueParameters());
+
+	}
+}

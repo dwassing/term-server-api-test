6ddb1826a30552f517fe8b19e95ebb167bdbdbb3
Daniel Wassing <danwa223@student.liu.se>
2020-07-15 13:42:19 +0200

    Changed lookup of relevant element to last. See attached code commentary!

--- TermServerRestAPI/src/main/java/APItest/TermServerRestAPI/FHIRMapper.java
+++ TermServerRestAPI/src/main/java/APItest/TermServerRestAPI/FHIRMapper.java
@@ -31,8 +31,13 @@ public class FHIRMapper
 		// Parse it
 		Parameters parsed = parser.parseResource(Parameters.class, stringTomap);
 		List<ParametersParameterComponent> e = parsed.getParameter();
-		ParametersParameterComponent rr = e.get(1);
+		ParametersParameterComponent rr = e.get(e.size() - 1);
+		//About the above line: so far in test cases we returned the LAST relevant element, not sure if this is
+		//true for all FHIR endpoints. This needs to be considered, maybe we need to pass location of element in
+		//json object if we find outlying examples. Passing locations of elements within json objects is a deeply
+		//discouraged practice however and should only be considered as a very last resort.
 		String result = rr.getValue().toString();
+		
 		return result;
 	}	
 }

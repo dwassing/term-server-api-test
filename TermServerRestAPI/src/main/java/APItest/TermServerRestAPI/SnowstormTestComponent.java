0b773677d176e2f7e34030c61d4c84cc283f1d2c
Daniel Wassing <danwa223@student.liu.se>
2020-07-15 13:47:09 +0200

    Deleted bad string returns. Adding correct ones later.

--- TermServerRestAPI/src/main/java/APItest/TermServerRestAPI/SnowstormTestComponent.java
+++ TermServerRestAPI/src/main/java/APItest/TermServerRestAPI/SnowstormTestComponent.java
@@ -10,6 +10,8 @@ public class SnowstormTestComponent {
 			return "SNOMED CT"; //TODO: ADD KEY VALUE HERE
 		case "concept-lookup":
 			return "FHIR";
+		case "concept-subsumption":
+			return "FHIR";
 		}
 		return "";
 	}
@@ -20,17 +22,22 @@ public class SnowstormTestComponent {
 		case "concept-query":
 			return "ADD-ENDPOINT-PATH-HERE";
 		case "concept-lookup":
-			return "fhir/CodeSystem/"; //TODO: add path of FHIR lookup
+			return ""; //TODO: add path of FHIR lookup
+		case "concept-subsumption":
+			return "";
 		}
 		return "";
 	}
 	
-	public String getEndpointInfo(String queryType, int code) {
+	//returns additional information such as extra parameters, codeB is randomly generated currently.
+	public String getEndpointInfo(String queryType, int codeA, int codeB) {
 		switch(queryType) {
 		case "concept-query":
 			return ""; //TODO: maybe return the ID here. Currently done in main class.
 		case "concept-lookup":
 			return ""; //TODO: add path of FHIR lookup
+		case "concept-subsumption":
+			return "";
 		}
 		return "";
 	}
@@ -39,9 +46,11 @@ public class SnowstormTestComponent {
 	public String getInterestingJsonKeyValues(String queryType, int code) {
 		switch(queryType) {
 		case "concept-query":
-			return "term"; //TODO: ADD KEY VALUE HERE
+			return ""; //TODO: ADD KEY VALUE HERE
 		case "concept-lookup":
-			return "valueString";
+			return "";
+		case "concept-subsumption":
+			return "";
 		}
 		return "";
 	}

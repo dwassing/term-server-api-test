b08ff752238230b2bf89eaa53263eb1267aa067c
Daniel Wassing <danwa223@student.liu.se>
2020-08-14 09:44:16 +0200

    Cleaned up the FHIR Mapper.

--- TermServerRestAPI/src/main/java/APItest/TermServerRestAPI/FHIRMapper.java
+++ TermServerRestAPI/src/main/java/APItest/TermServerRestAPI/FHIRMapper.java
@@ -37,21 +37,18 @@ public class FHIRMapper
 		if (this.soughtIndex == 999) { //if testing
 			Parameters parsed = parser.parseResource(Parameters.class, stringToMap);
 			List<ParametersParameterComponent> e = parsed.getParameter();
-			rr = e.get(0); //e.size() - 1
+			rr = e.get(0);
 			System.out.println(rr);
-			//About the above line: so far in test cases we returned the LAST relevant element, not sure if this is
-			//true for all FHIR endpoints. This needs to be considered, maybe we need to pass location of element in
-			//json object if we find outlying examples. Passing locations of elements within json objects is a deeply
-			//discouraged practice however and should only be considered as a very last resort.
 		} else { //if ran against snowowl or snowstorm.
-			System.out.println(stringToMap);
 			Parameters parsed = parser.parseResource(Parameters.class, stringToMap);
 			List<ParametersParameterComponent> e = parsed.getParameter();
-			for (int i = 0; i < e.size(); i++) {
-				System.out.println(e.get(i).getValue().toString());
-			}
 			rr = e.get(this.soughtIndex);
 			result = rr.getValue().toString();
+			
+			//Below piece of code is a debugger to post all items returned by the IParser.
+			/**for (int i = 0; i < e.size(); i++) {
+				System.out.println(e.get(i).getValue().toString());
+			}**/
 		}
 		
 		return result;
